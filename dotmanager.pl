3c320fe435c876c94cb5f2da8df3a144ff7e4fc3
aajjbb <jefersonlsiq@gmail.com>
2015-12-29 16:08:26 -0200

    updating files hash with my current tracked files

--- dotmanager.pl
+++ dotmanager.pl
@@ -11,17 +11,28 @@ my $root = $ENV{'HOME'};
 my $current_dir = $ENV{'PWD'};
 
 my %files = (
-	"awesome" => $root . "/.config",
-	"irc.conf" => $root . "/.weechat",
-	"weechat.conf" => $root . "/.weechat",
+	#awesome
+	"freedesktop" => $root . "/.config/awesome",
+	"scratchdrop" => $root . "/.config/awesome",
+	"starbreaker" => $root . "/.config/awesome/themes",
+	
+	"menugen.lua" => $root . "/.config/awesome",
+	"rc.lua"      => $root . "/.config/awesome",
+	"widgets.lua" => $root . "/.config/awesome",
+	"xrandr.lua"  => $root . "/.config/awesome",
+
+	#weechat
+	".weechat/irc.conf" => $root . "/weechat",
+	".weechat/plugins.conf" => $root . "/weechat",
+
+	#general
 	".bash_profile" => $root,
 	".bashrc" => $root,
 	".emacs" => $root,
-	".emacs.d" => $root,
 	".gitconfig" => $root,
 	".tmux.conf" => $root,
 	".vimrc" => $root,
-	".Xdefaults" => $root,
+	".xbindkeysrc" => $root,	
 	".Xresources" => $root,	
 );
 
