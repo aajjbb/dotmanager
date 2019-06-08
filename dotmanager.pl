3fba7517ab7eeeb743bf4551033cf931573eeebf
Jeferson Siqueira <jefersonlsiq@gmail.com>
2019-06-08 13:58:10 +0100

    Changes the structure of --install by using symbolic links instead of file copying. This makes file maintance easier by altering them directly, instead of changing on the repository to then, install the new changes.
    With that, the old update function has been removed.
    
    This history might look ugly, but this is due to the changes being made a few weeks ago in my dotfile repo.
    Then, I just copied a new dotmanager.pl file instead of applying changes to the current version. As this
    is a personal repo, it does not matter much.

--- dotmanager.pl
+++ dotmanager.pl
@@ -1,21 +1,42 @@
 #!/usr/bin/perl
 
-package Dotmanager;
-
 use strict;
 use warnings;
 
 use feature 'say';
 
-my $base_dir = $ENV{'HOME'};
+my $root = $ENV{'HOME'};
 my $current_dir = $ENV{'PWD'};
 
-=pod
-%file maps files/folder to their right place in the file system
-=cut
-
 my %files = (
-    ".emacs" => $base_dir,
+    # awesome-wm
+    # awesome depends on other packages:
+    # (awesome-copycats, awesome-revelation, freedesktop)
+    ".config/awesome/rc.lua"        => $root,
+    ".config/awesome/xrandr.lua"    => $root,
+
+    # i3 config
+    ".config/i3/config"   => $root,
+    ".config/i3/init.sh"  => $root,
+
+    # kitty config
+    ".config/kitty/kitty.conf" => $root,
+
+    #general
+    ".bash_profile"         => $root,
+    ".bashrc"               => $root,
+    ".config/.compton.conf" => $root,
+    ".emacs"                => $root,
+    ".gitconfig"            => $root,
+    ".spacemacs"            => $root,
+    ".tmux.conf"            => $root,
+    ".vimrc"                => $root,
+    ".Xresources"           => $root,
+    ".zshrc"                => $root,
+
+    #scripts
+    ".bin/lock.sh"      => $root,
+    ".bin/suspend.sh" => $root,
 );
 
 =doc
@@ -32,18 +53,23 @@ sub prompt {
 
 =doc
     Install dotfiles of the current repository for the current user in the system
+
+	In the first version, it used to happen by copying the files to the destination
+	directory. Now, in a wiser version, it just creates a symbolic link. With that,
+	I can now edit them freely without caring whether they are the original ones
+	or the ones in the repository.
 =cut
 
 sub install {
     my $skip = shift;
-    
+
     print("Starting the Installing Process \n");
 
     foreach my $key (keys %files) {
         my $source_file = $current_dir . "/" . $key;
         my $destiny_file = $files{$key} . "/" . $key;
 
-        if (not $skip) {        
+        if (not $skip) {
             my $answer = prompt("Are you sure you want to install $key ? [y/n]");
 
             if (not ($answer eq 'y' || $answer eq 'Y')) {
@@ -51,65 +77,32 @@ sub install {
                 next;
             }
         }
-        
+
         print("Installing " . $key . " in " . $destiny_file . "\n");
-        
-        my $result = `cp -TR $source_file $destiny_file`;
-        
+
+        my $result = `ln -s $source_file $destiny_file`;
+
         if ($result) {
             say "Failed to install " . $key . " is probably being used by other application or it does not exists";
-        }    
-    }
-}
-
-=doc
-    Updates the current system dotfiles to the repository
-=cut
-
-sub update {
-    my $skip = shift;
-    print("Starting the Uploading Process\n");
-
-    foreach my $key (keys %files) {
-        my $source_file = $files{$key} . "/" . $key;
-        my $destiny_file = $current_dir . "/" . $key;
-
-        if (not $skip) {            
-            my $answer = prompt("Are you sure you want to upload $key ? [y/n]");
-
-            if (not ($answer eq 'y' || $answer eq 'Y')) {
-                print("Skipping $key update\n");
-                next;
-            }
-        }
-        print("Uploading " . $source_file . " in " . $destiny_file . "\n");
-        
-        my $result = `cp -TR $source_file $destiny_file`;
-        
-        if ($result) {
-            print("Failed to update " . $key . " is probably being used by other application  or it does not exists\n");
         }
     }
 }
 
 my $usage = "Usage: perl dotmanager.pl [OPTION] [BYPASS]\n
-  Options: 
-    --install [Install current files in repository to system]\n
-    --update [Update files in repository with ones from the system]\n
+  Options:
+    install [Install current files in repository to system by creating symbolic links]\n
   Bypass:
     --skip [Skip Confirmations]\n
     ";
-    
-if ($#ARGV == 0 || $#ARGV > 2) {
+
+if ($#ARGV > 2) {
     print($usage);
 } else {
     my $option = $ARGV[0];
     my $bypass = ($ARGV[1] and ($ARGV[1] eq "--skip")) or 0;
-    
-    if ($option eq "--install") {
+
+    if ($option eq "install") {
         install($bypass);
-    } elsif ($option eq "--update") {
-        update($bypass);
     } else {
         print("Invalid argument\n$usage");
     }
